7e46d6421152f1d6fe2277af8c7bfe2729570397
Alina <malinka775@yandex.ru>
2022-05-20 11:20:12 +0300

    feat: do third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+async function getDirFiles (pathToDir) {
+  try {
+    const dirents = await fs.promises.readdir(pathToDir, {withFileTypes: true});
+    for (const dirent of dirents) {
+      if (dirent.isFile()) {
+        const {ext, name} = path.parse(path.join(pathToDir, dirent.name));
+        fs.stat(path.join(pathToDir, dirent.name), (err, stats) => {
+          if (err) console.log(err);
+          console.log(`${name} - ${ext.slice(1)} - ${stats.size}b`);
+        });
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+getDirFiles(path.join(__dirname, 'secret-folder'));
