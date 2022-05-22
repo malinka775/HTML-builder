e4217aa4e71680948692aa8ba8da627910b2139a
Alina <malinka775@yandex.ru>
2022-05-22 19:24:28 +0300

    feat: task 5, stable

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+async function mergeStyles () {
+  const dirents = await fs.promises.readdir(path.join(__dirname, 'styles'));
+  const styleDirents = dirents.filter(dirent => 
+    ( path.parse( path.join( __dirname, 'styles', dirent ) ) ).ext === '.css');
+  const writeStream = fs.createWriteStream(path.join( __dirname, 'project-dist', 'bundle.css' ), 'utf-8');
+  styleDirents.forEach(async (dirent) => {
+    
+    const readStream = fs.createReadStream(path.join( __dirname, 'styles', dirent ));
+    readStream.pipe(writeStream);
+    
+  });
+  
+}
+
+mergeStyles();
