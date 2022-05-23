b248f8f8a4920df8e0bfba18d7c8cc26c81f1d4f
Alina <malinka775@yandex.ru>
2022-05-23 22:36:42 +0300

    feat: do 6-th task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -6,10 +6,10 @@ async function mergeStyles () {
   const styleDirents = dirents.filter(dirent => 
     ( path.parse( path.join( __dirname, 'styles', dirent ) ) ).ext === '.css');
   const writeStream = fs.createWriteStream(path.join( __dirname, 'project-dist', 'bundle.css' ), 'utf-8');
-  styleDirents.forEach(async (dirent) => {
+  for (let dirent of styleDirents) {
     const readStream = fs.createReadStream(path.join( __dirname, 'styles', dirent ));
     readStream.pipe(writeStream);
-  });
+  }
   
 }
 
---
+++ 06-build-page/index.js
@@ -0,0 +1,113 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
+
+
+async function getContent (pathToFile) {
+  return fsPromises.readFile(pathToFile, 'utf-8', async (error, fileContent) => {
+    if (error) console.log(error.message);
+    return await fileContent;
+  });
+}
+async function getComponents() {
+  const dirents = await fsPromises.readdir(path.join(__dirname, 'components'));
+  const direntsContent = [];
+  for (let dirent of dirents) {
+    const {name} = path.parse(path.join(__dirname, 'components', dirent));
+    const content = await getContent(path.join(__dirname, 'components', dirent));
+    const obj = {
+      name,
+      content
+    };
+    direntsContent.push(obj);
+  }
+  return direntsContent;
+}
+
+const mergeHtml = async ()=> {
+  const componentsContent = await getComponents();
+  let newFile = await getContent(path.join(__dirname, 'template.html'));
+  for (let component of componentsContent) {
+    newFile = newFile.replace(`{{${component.name}}}`, `\n ${component.content} \n`);
+    
+  }
+  await fsPromises.mkdir(path.join(__dirname, 'project-dist'), {recursive: true});
+  fs.writeFile((path.join(__dirname, 'project-dist', 'index.html')), newFile, (err) => {
+    if (err) return console.log(err.message);
+  });
+};
+
+async function mergeStyles () {
+  const dirents = await fs.promises.readdir(path.join(__dirname, 'styles'));
+  const styleDirents = dirents.filter(dirent => 
+    ( path.parse( path.join( __dirname, 'styles', dirent ) ) ).ext === '.css');
+  const writeStream = fs.createWriteStream(path.join( __dirname, 'project-dist', 'style.css' ), 'utf-8');
+  styleDirents.forEach(async (dirent) => {
+    const readStream = fs.createReadStream(path.join( __dirname, 'styles', dirent ));
+    readStream.pipe(writeStream);
+  });
+  
+}
+async function deleteDirectory(pathToDir) {
+  const dirents = await fsPromises.readdir(pathToDir, {withFileTypes: true});
+  for (const dirent of dirents) {
+    if (dirent.isFile()) {
+      fs.rm(path.join(pathToDir, dirent.name), { recursive:true, force: true  }, err => {
+        if (err) {console.log(err.message);}
+      });
+    } else if (dirent.isDirectory()){
+      const innerDirents = await fsPromises.readdir(path.join(pathToDir, dirent.name), {withFileTypes: true});
+      if(innerDirents.length) {
+        await deleteDirectory(path.join(pathToDir, dirent.name));
+      } else {
+        fs.rm(path.join(pathToDir, dirent.name), { recursive:true, force: true  }, err => {
+          if (err) {console.log(err.message);}
+        });
+      }
+      
+    } 
+  }
+}
+
+
+async function copyDirFiles (initialPath, destinationPath) {
+  
+  try {
+    
+    await fsPromises.mkdir(destinationPath, {recursive: true});
+    const dirents = await fsPromises.readdir(initialPath, {withFileTypes: true});
+    let direntNames = [];
+    for (const dirent of dirents) {
+      if (dirent.isFile()) {
+        direntNames.push(dirent.name);
+        const rs = fs.createReadStream(path.join(initialPath, dirent.name));
+        const ws = fs.createWriteStream(path.join(destinationPath, dirent.name));
+        rs.pipe(ws);
+      } else {
+        const sourcePath = path.join(initialPath, dirent.name);
+        const destPath = path.join(destinationPath, dirent.name);
+        copyDirFiles (sourcePath, destPath);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+try {
+  
+  (async () => {
+    await mergeHtml();
+    const destinationPath = path.join(__dirname, 'project-dist', 'assets');
+    const sourcePath = path.join(__dirname, 'assets');
+    fs.access(destinationPath, async function(error) {
+      if (!error) {
+        await deleteDirectory(destinationPath);
+      } 
+    });
+    await mergeStyles();
+    await copyDirFiles(sourcePath,destinationPath);
+  })();
+} catch (error) {
+  console.error(error);
+}
+
